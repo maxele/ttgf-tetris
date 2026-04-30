64f24f359c19f36aee682cd764bd3b8b863ed015
Max Masiero <max.masiero@student.tugraz.at>
2026-04-30 15:11:29 +0200

    Removed obj_dir in testbench

--- tb/obj_dir/tb.d
+++
@@ -1,8 +0,0 @@
-tb.o: ../tb.cpp Vtt_um_maxele.h /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilated_config.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_types.h \
- /usr/share/verilator/include/verilated_funcs.h \
- /usr/share/verilator/include/verilated_vcd_c.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilated_trace.h
